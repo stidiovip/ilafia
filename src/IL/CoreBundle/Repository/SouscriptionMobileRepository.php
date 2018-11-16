aea7d02bcfad7bfcde3a046f3392fecd7a38fcfa
Tidiane SIDIBE <stidiovip@yahoo.fr>
2018-11-16 17:23:23 +0000

    Fixes

--- src/IL/CoreBundle/Repository/SouscriptionMobileRepository.php
+++ src/IL/CoreBundle/Repository/SouscriptionMobileRepository.php
@@ -31,35 +31,31 @@ class SouscriptionMobileRepository extends \Doctrine\ORM\EntityRepository
         $qb = null;
         $filters = [];
 
-        if($operateur){
-            $qb = $this->createQueryBuilder('s');
+        $qb = $this->createQueryBuilder('s');
+        
+        if ( $operateur != null ){
+            $qb = $qb->andWhere('s.operateur = :operateur');
+            $filters['operateur'] = $operateur;
+        }
+
+        if ( $statutLiaison != null ){
+            $qb = $qb->andWhere('s.statutLiaison = :statutLiaison');
+            $filters['statutLiaison'] = $statutLiaison;
+        }
+
+        if ( $dateDebut != null && $dateFin != null ){
+
+            $dd = \DateTime::createFromFormat('Y-m-d H:i', $dateDebut . '00:00');
+            $df = \DateTime::createFromFormat('Y-m-d H:i', $dateFin . '23:59');
             
-            if ( $operateur != null ){
-                $qb = $qb->andWhere('s.operateur = :operateur');
-                $filters['operateur'] = $operateur;
-            }
-
-            if ( $statutLiaison != null ){
-                $qb = $qb->andWhere('s.statutLiaison = :statutLiaison');
-                $filters['statutLiaison'] = $statutLiaison;
-            }
-
-            if ( $dateDebut != null && $dateFin != null ){
-
-                $dd = \DateTime::createFromFormat('d/m/Y H:i', $dateDebut . '00:00');
-                $df = \DateTime::createFromFormat('d/m/Y H:i', $dateFin . '23:59');
-                
-                $qb = $qb->andWhere('s.createdAt BETWEEN :dateDebut AND :dateFin');
-                $filters['dateDebut'] = $dd;
-                $filters['dateFin'] = $df;
-
-            }
-
-            $qb = $qb->orderBy('s.createdAt', 'ASC')->setParameters($filters)->getQuery();
-
-            return $qb->getResult();
+            $qb = $qb->andWhere('s.createdAt BETWEEN :dateDebut AND :dateFin');
+            $filters['dateDebut'] = $dd;
+            $filters['dateFin'] = $df;
 
         }
 
+        $qb = $qb->orderBy('s.createdAt', 'ASC')->setParameters($filters)->getQuery();
+
+        return $qb->getResult();
     }
 }
